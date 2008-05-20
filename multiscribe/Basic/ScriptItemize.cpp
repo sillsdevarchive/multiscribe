e1efaa23d36bb51bbfba3abdeec8dd4097f1125a
Sharon Correll <correll@dallas.sil.org>
2008-05-20 19:15:41 +0000

    Trivial changes.

--- multiscribe/Basic/ScriptItemize.cpp
+++ multiscribe/Basic/ScriptItemize.cpp
@@ -1,8 +1,9 @@
 #pragma comment(linker, "/export:ScriptItemize=_usp10.ScriptItemize")
 
+#include "../stdafx.h"
+
 #ifdef INTERCEPT_SCRIPTITEMIZE
 
-#include "../stdafx.h"
 //int GetGraphiteScriptId();
 const SCRIPT_PROPERTIES * GetScriptPropertiesOfEngine(int i);
 LPVOID GetOriginalScriptItemize();
@@ -34,28 +35,30 @@ __checkReturn HRESULT WINAPI GraphiteEnabledScriptItemize(
 	__in_ecount_opt(1) const SCRIPT_CONTROL             *psControl,     // In   Analysis control (optional)
 	__in_ecount_opt(1) const SCRIPT_STATE               *psState,       // In   Initial bidi algorithm state (optional)
 	__out_ecount_part(cMaxItems, *pcItems) SCRIPT_ITEM  *pItems,        // Out  Array to receive itemization
-	__out_ecount(1) int                                 *pcItems)      // Out  Count of items processed (optional)
+	__out_ecount(1) int                                 *pcItems)       // Out  Count of items processed (optional)
 {
 	//WRAP_BEGIN(ScriptItemize, LPFNSCRIPTITEMIZE)
-  LPFNSCRIPTITEMIZE ScriptItemize = (LPFNSCRIPTITEMIZE) GetOriginalScriptItemize();
-  const WCHAR* s = pwcInChars;
+	LPFNSCRIPTITEMIZE ScriptItemize = (LPFNSCRIPTITEMIZE) GetOriginalScriptItemize();
+	const WCHAR* s = pwcInChars;
 
-  //WCHAR* s = new WCHAR[cInChars];
-  //if(pwcInChars != NULL){
-  //  for(int i = 0; i < cInChars; ++i){
-  //    WCHAR w = pwcInChars[i];
-  //    if(0x07C0 <= w && w <= 0x07ff) { //N'Ko
-  //        w = L'س'; // fool it into thinking we're arabic!
-  //    }
-  //    s[i] = w;
-  //  }
-  //}
+	//WCHAR* s = new WCHAR[cInChars];
+	//if (pwcInChars != NULL) {
+	//	for (int i = 0; i < cInChars; ++i) {
+	//		WCHAR w = pwcInChars[i];
+	//		if (0x07C0 <= w && w <= 0x07ff) { //N'Ko
+	//			w = L'س'; // fool it into thinking we're arabic!
+	//		}
+	//		s[i] = w;
+	//	}
+	//}
 
 	HRESULT hResult = ScriptItemize(s, cInChars, cMaxItems, psControl, psState, pItems, pcItems);
- // delete[] s;
-	//need to make sure SCRIPT_ITEM.SCRIPT_ANALYSIS.eScript initialized to
-	// be a complex script if we are going to get called.
-	//for(int i=0; i < *pcItems; ++i){
+
+	//delete[] s;
+
+	//Need to make sure SCRIPT_ITEM.SCRIPT_ANALYSIS.eScript initialized to
+	//be a complex script if we are going to get called.
+	//for (int i=0; i < *pcItems; ++i) {
 	//	SCRIPT_PROPERTIES * sp = GetScriptPropertiesOfEngine(pItems[i].a.eScript);
 	//	if (sp->langid != SCRIPT_UNDEFINED &&
 	//		!sp->fComplex){
@@ -65,34 +68,35 @@ __checkReturn HRESULT WINAPI GraphiteEnabledScriptItemize(
 	//WRAP_END
 	//}
 
-  //bool fNko=false;
-  //for(int i = 0; i < *pcItems; ++i){
-  //  WCHAR startChar = pwcInChars[pItems[i].iCharPos];
-  //  if(0x07C0 <= startChar && startChar <= 0x07ff) { //N'Ko
-  //    if(eScriptArabic == -1){
-  //      SCRIPT_ITEM rgItems[2];
-  //      int cItemsProcessed=0;
-  //      wchar_t arabic = L'س';
-  //      HRESULT hResult = ScriptItemize(&arabic, 1, 2, NULL, NULL, (SCRIPT_ITEM*)&rgItems, &cItemsProcessed);
-  //      if(hResult==S_OK){
-  //        eScriptArabic = rgItems[0].a.eScript;
-  //      }
-  //    }
-  //    pItems[i].a.fRTL = true;
-  //    pItems[i].a.fLayoutRTL = true;
-  //    pItems[i].a.s.uBidiLevel = 1;
-  //    fNko = true;
-  //    if(eScriptArabic != -1){
-  //     // pItems[i].a.eScript = eScriptArabic; // fool it into thinking we're arabic!
-  //    }
-  //  }
-//       const SCRIPT_PROPERTIES * sp = GetScriptPropertiesOfEngine(pItems[i].a.eScript);
-//assert(sp);
- //}
- // if(fNko){
- //   *pcItems = 1;
- // }
-  return hResult;
+	//bool fNko=false;
+	//for (int i = 0; i < *pcItems; ++i) {
+	//  WCHAR startChar = pwcInChars[pItems[i].iCharPos];
+	//	if (0x07C0 <= startChar && startChar <= 0x07ff) { //N'Ko
+	//		if (eScriptArabic == -1) {
+	//			SCRIPT_ITEM rgItems[2];
+	//			int cItemsProcessed=0;
+	//			wchar_t arabic = L'س';
+	//			HRESULT hResult = ScriptItemize(&arabic, 1, 2, NULL, NULL, (SCRIPT_ITEM*)&rgItems, &cItemsProcessed);
+	//			if (hResult==S_OK) {
+	//				eScriptArabic = rgItems[0].a.eScript;
+	//			}
+	//		}
+	//		pItems[i].a.fRTL = true;
+	//		pItems[i].a.fLayoutRTL = true;
+	//		pItems[i].a.s.uBidiLevel = 1;
+	//		fNko = true;
+	//		if (eScriptArabic != -1) {
+	//			// pItems[i].a.eScript = eScriptArabic; // fool it into thinking we're arabic!
+	//		}
+	//	}
+	//	const SCRIPT_PROPERTIES * sp = GetScriptPropertiesOfEngine(pItems[i].a.eScript);
+	//	assert(sp);
+	//}
+	//if (fNko) {
+	//	*pcItems = 1;
+	//}
+
+	return hResult;
 }
 
 //#ifdef __cplusplus
@@ -100,4 +104,4 @@ __checkReturn HRESULT WINAPI GraphiteEnabledScriptItemize(
 //#endif
 
 
-#endif
+#endif // INTERCEPT_SCRIPTITEMIZE
